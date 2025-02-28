4a28c36f33834042aed2f70b5489e81023281f2d
Harmony Mothershed <112271331+hmothershed@users.noreply.github.com>
2025-02-28 12:28:03 -0600

    Update script.js

--- 03-CSS-Variables/script.js
+++ 03-CSS-Variables/script.js
@@ -1,1 +1,30 @@
+const inputs = document.querySelectorAll('.controls input');
+function handleUpdate(){
+  const suffix = this.dataset.sizing || '';
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
 
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('input', handleUpdate)); 
+
+const photo = document.querySelector('.photo');
+
+photo.addEventListener('click', resetValues);
+
+const root = document.documentElement;
+const styles = getComputedStyle(root);
+const baseColor = styles.getPropertyValue('--base');
+
+function resetValues() {
+  inputs.forEach(input => {
+    console.log(input.type);
+    if (input.type === 'range') {
+      input.value = 0;
+      document.documentElement.style.setProperty(`--${input.name}`, '0');
+    }
+    else {
+      input.value = baseColor;
+      document.documentElement.style.setProperty(`--${input.name}`, baseColor);
+    }
+  });
+}
