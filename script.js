aa32ab9d9592443ca7d8e68d907c5e166ab7d722
Harmony Mothershed <112271331+hmothershed@users.noreply.github.com>
2025-03-24 10:46:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -73,8 +73,8 @@ const challenges = [
         id: 11,
         title: "html5 video player",
         image: "./assets/in_progress.png",
-        demo: "",
-        repo: ""
+        demo: "https://hmothershed.github.io/JavaScript30/11-Custom-Video-Player/",
+        repo: "https://github.com/hmothershed/JavaScript30/tree/main/11-Custom-Video-Player"
       },
       {
         id: 12,
