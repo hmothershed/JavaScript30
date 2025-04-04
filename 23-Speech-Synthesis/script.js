0404cb48ba09bc0f69852eb3946576637dc2a535
Harmony Mothershed <112271331+hmothershed@users.noreply.github.com>
2025-04-04 11:04:00 -0500

    Add files via upload

---
+++ 23-Speech-Synthesis/script.js
@@ -0,0 +1,69 @@
+const msg = new SpeechSynthesisUtterance();
+let voices = [];
+const voicesDropdown = document.querySelector('[name="voice"]');
+const options = document.querySelectorAll('[type="range"], [name="text"]');
+const speakButton = document.querySelector("#speak");
+const stopButton = document.querySelector("#stop");
+msg.text = document.querySelector('[name="text"]').value;
+
+const rateSlider = document.querySelector('[name="rate"]');
+const pitchSlider = document.querySelector('[name="pitch"]');
+const rateValue = document.querySelector("rateValue");
+const pitchValue = document.querySelector("pitchValue");
+
+// function to update slider values
+function updateValue(event) {
+  event.target.nextElementSibling.textContent = event.target.value;
+}
+
+function populateVoices() {
+  /*voices = this.getVoices();
+  voicesDropdown.innerHTML = voices
+    .filter(voice => voice.lang.includes('en'))
+    .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
+    .join('');*/
+  if (typeof speechSynthesis === "undefined") {
+    return;
+  }
+
+  voices = speechSynthesis.getVoices();
+
+  for (let i = 0; i < voices.length; i++) {
+    const option = document.createElement("option");
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+    if (voices[i].default) {
+      option.textContent += " — DEFAULT";
+    }
+
+    option.setAttribute("data-lang", voices[i].lang);
+    option.setAttribute("data-name", voices[i].name);
+    voicesDropdown.appendChild(option);
+  }
+}
+
+function setVoice() {
+  msg.voice = voices.find((voice) => voice.name === this.value);
+  toggle();
+}
+
+function toggle(startOver = true) {
+  speechSynthesis.cancel();
+  if (startOver) {
+    speechSynthesis.speak(msg);
+  }
+}
+
+function setOption() {
+  console.log(this.name, this.value);
+  msg[this.name] = this.value;
+  toggle();
+}
+
+speechSynthesis.addEventListener("voiceschanged", populateVoices);
+voicesDropdown.addEventListener("change", setVoice);
+options.forEach((option) => option.addEventListener("change", setOption));
+speakButton.addEventListener("click", toggle);
+stopButton.addEventListener("click", () => toggle(false));
+rateSlider.addEventListener("input", updateValue);
+pitchSlider.addEventListener("input", updateValue);
